e2e39d2a0b27d83f94b8957f93eb1d702e674e5d
John Wass <jwass3@gmail.com>
2020-11-25 13:29:35 -0500

    initial import

---
+++ conanfile.py
@@ -0,0 +1,19 @@
+from conans import ConanFile
+
+
+class ParticlePackage(ConanFile):
+    name = 'ssd1306'
+    version = '2.4.1'
+    url = 'https://github.com/hicktech/conan-ssd1306'
+    repo_url = 'https://github.com/adafruit/Adafruit_SSD1306.git'
+    generators = 'cmake'
+    settings = []
+    requires = []
+
+    def package(self):
+        self.copy('*.c*', dst='src')
+        self.copy('*.h*', dst='include')
+
+    def source(self):
+        self.run(f'git clone {self.repo_url} .')
+        self.run(f'git checkout {self.version}')
